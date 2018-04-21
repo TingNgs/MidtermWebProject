6d01b3286f2cbfa6def6ec715ac48117929147d6
ngSgn <t105590049@ntut.org.tw>
2018-04-22 02:05:56 +0800

    Add:     add new product script

--- js/script.js
+++ js/script.js
@@ -2,3 +2,29 @@ $(function() {$("#footer").load("footer.html");
     $("#header").load("header.html");
     
 });
+
+function getNewProduct(){
+    $.ajax({
+        type: 'GET',
+        url: 'data.txt',
+        dataType: 'json',
+        success: function (response) {
+            $.each(response, function(index, element) {
+                var tempString =element.state;
+                if(tempString != undefined){
+                    if(tempString.indexOf("#new")>-1){
+                        $('#new_product').append(
+                            $('<div class="horizontal_product product"></div>').prepend(
+                                $('<img>',{src:element.path}),
+                                $('<p>',{text:element.name})
+                            )
+                        )
+                    }
+                }
+            });
+        },
+        error:function(xhr){
+            alert("發生錯誤: " + xhr.status + " " + xhr.statusText);
+        },
+    });
+};
