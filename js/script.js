5d80337421e7ee2e6af1b92907389f9d71da5536
ngSgn <t105590049@ntut.org.tw>
2018-04-22 06:09:05 +0800

    Add:     production dialog function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,48 @@
 $(function() {$("#footer").load("footer.html");
     $("#header").load("header.html");
-    
+    $( "#dialog" ).dialog({
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 100
+        },
+        hide: {
+            effect: "blind",
+            duration: 100
+        },
+        draggable: false,
+        resizable: false,
+        width: 1100,
+        height: 600
+    });
+    $(document).on('click', ".product", function () {
+        url= 'data.txt';
+        clicked = $(this);
+        $( "#dialog" ).dialog('option', 'title',clicked );
+        $.get(url, function(index) {
+            var temp = index.toString();
+            var temp1 = JSON.parse("[" + temp + "]");
+            var json = temp1[0];
+            for(var i=0;i<json.length;i++){
+                if(clicked[0].childNodes[1].innerHTML == json[i].name){
+                    console.log(json[i])
+                    $('#dialog').empty();
+                    $('#dialog').append(
+                        $('<img id="dialog_img">').attr("src",json[i].path),
+                        $('<div id="dialog_info"></div>').append(
+                            $('<p>',{text:"Name : "+json[i].name}),
+                            $('<p>',{text:"Resolution : "+json[i].resolution}),
+                            $('<p>',{text:"Dimensions : "+json[i].dimensions}),
+                            $('<p>',{text:"Weight : "+json[i].weight}),
+                            $('<p>',{text:"Features : "+json[i].features})
+                        )
+                    )
+                    break;
+                }
+            }
+        });
+        $( "#dialog" ).dialog( "open" );
+    });
 });
 
 function getNewProduct(){
@@ -14,9 +56,12 @@ function getNewProduct(){
                 if(tempString != undefined){
                     if(tempString.indexOf("#new")>-1){
                         $('#new_product').append(
-                            $('<div class="horizontal_product product"></div>').prepend(
-                                $('<img>',{src:element.path}),
-                                $('<p>',{text:element.name})
+                            $('<a href="#"/>').append(
+                                $('<div class="horizontal_product product"></div>').append(
+                                    $('<img>',{src:element.path}),
+                                    $('<h2>',{text:element.name}),
+                                    $('<p>',{text:element.hover})
+                                )
                             )
                         )
                     }
